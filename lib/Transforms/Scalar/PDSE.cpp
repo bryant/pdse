aa4c1754fd1ea4611654e6e7caeae60c6e0047af
bryant <bryant@users.noreply.github.com>
2017-02-13 18:38:44 -0500

    reword BreakCriticalEdges explanation

--- lib/Transforms/Scalar/PDSE.cpp
+++ lib/Transforms/Scalar/PDSE.cpp
@@ -46,9 +46,9 @@
 
 #define DEBUG_TYPE "pdse"
 
-// TODO: Require BreakCriticalEdges which ensures that lambdas (resp. phis)
-// dominate (resp. post-dominate) their successor (resp. predecessor) blocks
-// (SSAPRE assumes this condition).
+// TODO: Require BreakCriticalEdges to ensure that lambdas (resp. phis) dominate
+// (resp. post-dominate) their successor (resp. predecessor) blocks (SSAPRE
+// assumes this condition).
 
 using namespace llvm;
 
